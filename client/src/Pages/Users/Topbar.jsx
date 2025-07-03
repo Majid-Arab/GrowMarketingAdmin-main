6d7cc55b7c6710a5383eba96f450ca8f1557e4f3
Abdul Majid <arabmajid12@gmail.com>
2025-07-04 04:16:21 +0500

    Setting up Create Client model into Topbar
    Separating Employee Model and Client Model for better understanding

--- client/src/Pages/Users/Topbar.jsx
+++ client/src/Pages/Users/Topbar.jsx
@@ -2,15 +2,21 @@ import React, { useEffect, useRef, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { Add, Close } from "@mui/icons-material";
 import { Path } from "../../utils";
-import { Chip, FormControl, Input, InputAdornment, Tooltip } from "@mui/material";
+import {
+  Chip,
+  FormControl,
+  Input,
+  InputAdornment,
+  Tooltip,
+} from "@mui/material";
 import { PiMagnifyingGlass } from "react-icons/pi";
 import { FiFilter } from "react-icons/fi";
 import CreateUser from "./CreateEmployee";
+import CreateClient from "./CreateClient";
 import Filter from "./Filter";
 import { searchUserReducer } from "../../redux/reducer/user";
 
 const Topbar = ({ view, setView, setIsFiltered, isFiltered }) => {
-
   ///////////////////////////////////////// VARIABLES ///////////////////////////////////////////////////
   const { pathname } = useLocation();
   const pathArr = pathname.split("/").filter((item) => item != "");
@@ -23,30 +29,36 @@ const Topbar = ({ view, setView, setIsFiltered, isFiltered }) => {
   const descriptionElementRef = useRef(null);
 
   ///////////////////////////////////////// STATES ///////////////////////////////////////////////////
-  const [open, setOpen] = useState(false);
+  const [openEmployeeModal, setOpenEmployeeModal] = useState(false);
+  const [openClientModal, setOpenClientModal] = useState(false);
   const [openFilters, setOpenFilters] = useState(false);
   const [scroll, setScroll] = useState("paper");
 
   ///////////////////////////////////////// USE EFFECTS ///////////////////////////////////////////////////
   useEffect(() => {
-    if (open) {
+    if (openEmployeeModal || openClientModal) {
       const { current: descriptionElement } = descriptionElementRef;
       if (descriptionElement != null) {
         descriptionElement.focus();
       }
     }
-  }, [open]);
+  }, [openEmployeeModal, openClientModal]);
 
   ///////////////////////////////////////// FUNCTIONS ///////////////////////////////////////////////////
   const handleSearch = (searchTerm) => {
     dispatch(searchUserReducer(searchTerm));
-  }
+  };
   const handleToggleFilters = () => {
     setOpenFilters((pre) => !pre);
   };
 
-  const handleCreateopen = (scrollType) => () => {
-    setOpen(true);
+  const handleCreateEmployeeOpen = (scrollType) => () => {
+    setOpenEmployeeModal(true);
+    setScroll(scrollType);
+  };
+
+  const handleCreateClientOpen = (scrollType) => () => {
+    setOpenClientModal(true);
     setScroll(scrollType);
   };
 
@@ -57,18 +69,19 @@ const Topbar = ({ view, setView, setIsFiltered, isFiltered }) => {
       </div>
 
       <div className="flex justify-between items-center mb-5">
-        <h1 className="text-primary-blue text-[32px] capitalize font-light">{title}</h1>
+        <h1 className="text-primary-blue text-[32px] capitalize font-light">
+          {title}
+        </h1>
 
         {showEmployeeTopBar && (
           <div className="flex items-center gap-2">
-            {
-              isFiltered &&
+            {isFiltered && (
               <Chip
                 label="Filtered"
                 onDelete={() => setIsFiltered(false)}
                 deleteIcon={<Close />}
               />
-            }
+            )}
             <div className="bg-[#ebf2f5] hover:bg-[#dfe6e8] p-1 pl-2 pr-2 rounded-md w-48">
               <FormControl>
                 <Input
@@ -85,16 +98,18 @@ const Topbar = ({ view, setView, setIsFiltered, isFiltered }) => {
             <Tooltip title="Filter" arrow placement="top">
               <div
                 onClick={handleToggleFilters}
-                className={` p-2 rounded-md cursor-pointer ${openFilters
-                  ? "text-[#20aee3] bg-[#e4f1ff]"
-                  : "bg-[#ebf2f5] hover:bg-[#dfe6e8] text-[#a6b5bd]"
-                  }`}>
+                className={` p-2 rounded-md cursor-pointer ${
+                  openFilters
+                    ? "text-[#20aee3] bg-[#e4f1ff]"
+                    : "bg-[#ebf2f5] hover:bg-[#dfe6e8] text-[#a6b5bd]"
+                }`}
+              >
                 <FiFilter className="text-[25px] " />
               </div>
             </Tooltip>
             <div>
               <Tooltip title="Add New Employee" placement="top" arrow>
-                <div onClick={handleCreateopen("body")}>
+                <div onClick={handleCreateEmployeeOpen("body")}>
                   <button className="bg-primary-red hover:bg-red-400 transition-all text-white w-[44px] h-[44px] flex justify-center items-center rounded-full shadow-xl">
                     <Add />
                   </button>
@@ -120,11 +135,33 @@ const Topbar = ({ view, setView, setIsFiltered, isFiltered }) => {
                 />
               </FormControl>
             </div>
+            <div>
+              <Tooltip title="Add New Client" placement="top" arrow>
+                <div onClick={handleCreateClientOpen("body")}>
+                  <button className="bg-primary-red hover:bg-red-400 transition-all text-white w-[44px] h-[44px] flex justify-center items-center rounded-full shadow-xl">
+                    <Add />
+                  </button>
+                </div>
+              </Tooltip>
+            </div>
           </div>
         )}
       </div>
-      <CreateUser open={open} scroll={scroll} setOpen={setOpen} />
-      <Filter open={openFilters} setOpen={setOpenFilters} setIsFiltered={setIsFiltered} />
+      <CreateUser
+        open={openEmployeeModal}
+        scroll={scroll}
+        setOpen={setOpenEmployeeModal}
+      />
+      <CreateClient
+        open={openClientModal}
+        scroll={scroll}
+        setOpen={setOpenClientModal}
+      />
+      <Filter
+        open={openFilters}
+        setOpen={setOpenFilters}
+        setIsFiltered={setIsFiltered}
+      />
     </div>
   );
 };
