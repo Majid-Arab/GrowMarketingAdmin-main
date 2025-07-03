6aca9981d068a293ab15e9489156c178de68d9bc
Abdul Majid <arabmajid12@gmail.com>
2025-07-04 04:22:40 +0500

    Edit client setup in main Client file
    Setup Filter, View, Delete, Edit modals

--- client/src/Pages/Users/Clients.jsx
+++ client/src/Pages/Users/Clients.jsx
@@ -1,16 +1,17 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Topbar from "./Topbar";
 import { Table } from "../../Components";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import { getClients, getEmployeeClients } from "../../redux/action/user";
+import { getClients } from "../../redux/action/user";
+import EditClient from "./EditClient";
 import { getClientsReducer, getUserReducer } from "../../redux/reducer/user";
 import { Tooltip, styled } from "@mui/material";
-import { PiDotsThreeOutlineThin, PiTrashLight } from "react-icons/pi";
-import { IoOpenOutline } from "react-icons/io5";
+import { PiTrashLight } from "react-icons/pi";
 import { CiEdit } from "react-icons/ci";
-import { Dropdown, Menu, MenuButton, MenuItem, menuItemClasses } from "@mui/base";
-import Filter from "./Filter";
+import {
+  MenuItem,
+  menuItemClasses,
+} from "@mui/base";
 import User from "./User";
 import DeleteClient from "./Delete";
 
@@ -49,9 +50,13 @@ const StyledListbox = styled("ul")(
       overflow: auto;
       outline: 0px;
       background: ${theme.palette.mode === "dark" ? grey[900] : "#fff"};
-      border: 1px solid ${theme.palette.mode === "dark" ? grey[700] : grey[200]};
+      border: 1px solid ${
+        theme.palette.mode === "dark" ? grey[700] : grey[200]
+      };
       color: ${theme.palette.mode === "dark" ? grey[300] : grey[900]};
-      box-shadow: 0px 4px 30px ${theme.palette.mode === "dark" ? grey[900] : grey[200]};
+      box-shadow: 0px 4px 30px ${
+        theme.palette.mode === "dark" ? grey[900] : grey[200]
+      };
       z-index: 1;
       `
 );
@@ -68,8 +73,12 @@ const StyledMenuItem = styled(MenuItem)(
       }
     
       &.${menuItemClasses.focusVisible} {
-        outline: 3px solid ${theme.palette.mode === "dark" ? blue[600] : blue[200]};
-        background-color: ${theme.palette.mode === "dark" ? grey[800] : grey[100]};
+        outline: 3px solid ${
+          theme.palette.mode === "dark" ? blue[600] : blue[200]
+        };
+        background-color: ${
+          theme.palette.mode === "dark" ? grey[800] : grey[100]
+        };
         color: ${theme.palette.mode === "dark" ? grey[300] : grey[900]};
       }
     
@@ -78,7 +87,9 @@ const StyledMenuItem = styled(MenuItem)(
       }
     
       &:hover:not(.${menuItemClasses.disabled}) {
-        background-color: ${theme.palette.mode === "dark" ? grey[800] : grey[100]};
+        background-color: ${
+          theme.palette.mode === "dark" ? grey[800] : grey[100]
+        };
         color: ${theme.palette.mode === "dark" ? grey[300] : grey[900]};
       }
       `
@@ -87,7 +98,9 @@ const StyledMenuItem = styled(MenuItem)(
 const Clients = () => {
   ////////////////////////////////////// VARIABLES /////////////////////////////////////
   const dispatch = useDispatch();
-  const { clients, isFetching, error, loggedUser } = useSelector((state) => state.user);
+  const { clients, isFetching, error, loggedUser } = useSelector(
+    (state) => state.user
+  );
   const columns = [
     {
       field: "uid",
@@ -116,21 +129,27 @@ const Clients = () => {
       headerName: "Client Username",
       headerClassName: "super-app-theme--header",
       width: "200",
-      renderCell: (params) => <div className="capitalize font-primary">{params.row.username}</div>,
+      renderCell: (params) => (
+        <div className="capitalize font-primary">{params.row.username}</div>
+      ),
     },
     {
       field: "phone",
       headerName: "Phone",
       headerClassName: "super-app-theme--header",
       width: "150",
-      renderCell: (params) => <div className="font-primary">{params.row.phone}</div>,
+      renderCell: (params) => (
+        <div className="font-primary">{params.row.phone}</div>
+      ),
     },
     {
       field: "email",
       headerName: "Email",
       headerClassName: "super-app-theme--header",
       width: "220",
-      renderCell: (params) => <div className="font-primary">{params.row?.email}</div>,
+      renderCell: (params) => (
+        <div className="font-primary">{params.row?.email}</div>
+      ),
     },
     {
       field: "action",
@@ -139,58 +158,77 @@ const Clients = () => {
       headerClassName: "super-app-theme--header",
       renderCell: (params) => (
         <div className="flex gap-[10px]">
-          {
-            loggedUser?.role != 'employee' &&
-            <Tooltip placement="top" title="Delete" arrow>
-              {" "}
-              <PiTrashLight
-                onClick={() => handleOpenDeleteModal(params.row._id)}
-                className="cursor-pointer text-red-500 text-[23px] hover:text-red-400"
-              />
-            </Tooltip>
-          }
+          <Tooltip placement="top" title="Delete" arrow>
+            {" "}
+            <PiTrashLight
+              onClick={() => handleOpenDeleteModal(params.row._id)}
+              className="cursor-pointer text-red-500 text-[23px] hover:text-red-400"
+            />
+          </Tooltip>
+          <Tooltip placement="top" title="Edit" arrow>
+            {" "}
+            <CiEdit
+              onClick={() => handleOpenEditModal(params.row)}
+              className="cursor-pointer text-green-500 text-[23px] hover:text-green-600"
+            />
+          </Tooltip>
         </div>
       ),
     },
   ];
 
-  ////////////////////////////////////// STATES ////////////////////////////////////////
+  /////////////////////////////////////// STATES ////////////////////////////////////////
   const [openEditModal, setOpenEditModal] = useState(false);
   const [openDeleteModal, setOpenDeleteModal] = useState(false);
   const [selectedUserId, setSelectedUserId] = useState("");
   const [openFilters, setOpenFilters] = useState("");
-  const [openUser, setOpenUser] = useState(false);
+  const [openView, setOpenView] = useState(false);
+  const [isFiltered, setIsFiltered] = useState(false);
 
-  ////////////////////////////////////// USE EFFECTS ////////////////////////////////////
+  /////////////////////////////////////// USE EFFECTS ////////////////////////////////////
   useEffect(() => {
-    loggedUser.role == 'employee'
-      ?
-      dispatch(getEmployeeClients())
-      :
-      dispatch(getClients());
+    loggedUser.role == "employee"
+      ? dispatch(getEmployeeClients())
+      : dispatch(getClients());
   }, []);
+  useEffect(() => {
+    if (!isFiltered) {
+      dispatch(getClientsReducer());
+    }
+  }, [isFiltered]);
 
-  ////////////////////////////////////// FUNCTIONS //////////////////////////////////////////
-  const handleClickOpen = () => {
-    setOpenUser(true);
+  /////////////////////////////////////// FUNCTIONS /////////////////////////////////////
+  const hanldeOpenViewModal = (taskId) => {
+    setSelectedUserId(taskId);
+    setOpenView(true);
   };
-  const handleOpenEditModal = (employee) => {
-    dispatch(getUserReducer(employee));
+  const handleOpenEditModal = (client) => {
+    console.log("Client data before dispatch:", client);
+    dispatch(getUserReducer(client));
     setOpenEditModal(true);
   };
-  const handleOpenDeleteModal = (userId) => {
-    setSelectedUserId(userId);
+  const handleOpenDeleteModal = (taskId) => {
+    setSelectedUserId(taskId);
     setOpenDeleteModal(true);
   };
 
   return (
     <div className="w-full">
+      <EditClient open={openEditModal} setOpen={setOpenEditModal} />
+      <DeleteClient
+        open={openDeleteModal}
+        setOpen={setOpenDeleteModal}
+        userId={selectedUserId}
+      />
+      <User open={openView} setOpen={setOpenView} />
 
-      <DeleteClient open={openDeleteModal} setOpen={setOpenDeleteModal} userId={selectedUserId} />
-      <Filter open={openFilters} setOpen={setOpenFilters} />
-      <User open={openUser} setOpen={setOpenUser} />
+      <Topbar
+        openFilters={openFilters}
+        setOpenFilters={setOpenFilters}
+        isFiltered={isFiltered}
+        setIsFiltered={setIsFiltered}
+      />
 
-      <Topbar />
       <Table
         rows={clients}
         columns={columns}
